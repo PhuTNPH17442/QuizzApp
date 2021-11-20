4d0f1f1ef54d6f66076cd159b7bd76387892df21
duongld2002 <77181259+duongld2002@users.noreply.github.com>
2021-11-20 08:45:34 +0700

    Le Duong commit

--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         String test = "Đây là dữ liệu mới(kientvph17296)";
+
+        //Le Dinh Duong commit
+        String name = "Lê Đình Dương - PH17450";
     }
 }
