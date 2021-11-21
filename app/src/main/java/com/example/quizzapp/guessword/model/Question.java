2b9d0c9199581d6237af1564e7affb497a59e830
= <=>
2021-11-21 22:03:26 +0700

    Trần Văn Kiên commit

---
+++ app/src/main/java/com/example/quizzapp/guessword/model/Question.java
@@ -0,0 +1,60 @@
+package com.example.quizzapp.guessword.model;
+
+public class Question {
+    private String id;
+    private String question;
+    private String answer;
+    private String theLoaiID;
+
+    public Question() {
+    }
+
+    public Question(String id, String question, String answer, String theLoaiID) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.theLoaiID = theLoaiID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getTheLoaiID() {
+        return theLoaiID;
+    }
+
+    public void setTheLoaiID(String theLoaiID) {
+        this.theLoaiID = theLoaiID;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id='" + id + '\'' +
+                ", question='" + question + '\'' +
+                ", answer='" + answer + '\'' +
+                ", theLoaiID='" + theLoaiID + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/quizzapp/guessword/model/TheLoai.java
@@ -0,0 +1,38 @@
+package com.example.quizzapp.guessword.model;
+
+public class TheLoai {
+    private String id;
+    private String tenTheLoai;
+
+    public TheLoai() {
+    }
+
+    public TheLoai(String id, String tenTheLoai) {
+        this.id = id;
+        this.tenTheLoai = tenTheLoai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTenTheLoai() {
+        return tenTheLoai;
+    }
+
+    public void setTenTheLoai(String tenTheLoai) {
+        this.tenTheLoai = tenTheLoai;
+    }
+
+    @Override
+    public String toString() {
+        return "TheLoai{" +
+                "id='" + id + '\'' +
+                ", tenTheLoai='" + tenTheLoai + '\'' +
+                '}';
+    }
+}
