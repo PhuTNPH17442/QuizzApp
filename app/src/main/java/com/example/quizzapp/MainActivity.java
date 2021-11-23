99172547a5adbf70e68e089327e5e7d811e56003
duongld2002 <77181259+duongld2002@users.noreply.github.com>
2021-11-23 20:14:50 +0700

    LeDuong commit ngay 23/11/2021

--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.quizzapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,58 @@ public class MainActivity extends AppCompatActivity {
 //        Toast.makeText(this, "Đào Mạnh Toàn", Toast.LENGTH_SHORT).show();
 //        //Le Dinh Duong commit
 //        String name = "Lê Đình Dương - PH17450";
+        Button btnPlay = findViewById(R.id.btn_play);
+        Button btnSetting = findViewById(R.id.btn_settings);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Bạn có muốn thoát?")
+                    .setNegativeButton("Không", null)
+                    .setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            setResult(RESULT_OK, new Intent().putExtra("Exit", true));
+                            finish();
+                        }
+                    }).create().show();
+
+        } else {
+
+            Toast.makeText(this, "Nhấn lại nút Back để thoát", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("BUGBUG","onStop() in PlayActivity");
+        finish();
+
     }
 }
---
+++ app/src/main/java/com/example/quizzapp/constants/CategoryConstants.java
@@ -0,0 +1,12 @@
+package com.example.quizzapp.constants;
+
+public class CategoryConstants {
+
+    public static final String ALL = "All";
+    public static final String HISTORY = "History";
+    public static final String MATHS = "Maths";
+    public static final String ENGLISH = "English";
+    public static final String GEOGRAPHY = "Geography";
+    public static final String SCIENCE = "Science";
+
+}
