d05ffb367f89afe5da2bb9494689a1ead6e6e4f6
duongld2002 <77181259+duongld2002@users.noreply.github.com>
2021-12-02 20:25:19 +0700

    LeDuong commit

---
+++ app/src/main/java/com/example/quizzapp/SettingPreferences.java
@@ -0,0 +1,29 @@
+package com.example.quizzapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+
+public class SettingPreferences {
+
+    private static final String SHOW_MUSIC_ONOFF = "show_music_enable_disable";
+
+    public static void setMusicEnableDisable(Context context, Boolean result) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putBoolean(SHOW_MUSIC_ONOFF, result);
+        prefsEditor.commit();
+
+    }
+
+    public static boolean getMusicEnableDisable(Context context) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(SHOW_MUSIC_ONOFF, Constants.DEFAULT_MUSIC_SETTING);
+
+    }
+
+}
