2dc4e2ea438e08810de8f329b796f1d589b834c6
duongld2002 <77181259+duongld2002@users.noreply.github.com>
2021-12-06 21:04:34 +0700

    LeDuong commit

---
+++ app/src/main/java/com/example/quizzapp/SettingActivity.java
@@ -0,0 +1,135 @@
+package com.example.quizzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+
+public class SettingActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private Switch mMusicCheckbox;
+    private Button btn_ok;
+    private boolean isMusicOn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        mContext = SettingActivity.this;
+        AppController.currentActivity = this;
+
+        initViews();
+
+        populateMusicEnableContents();
+    }
+
+    private void initViews() {
+
+        mMusicCheckbox = findViewById(R.id.music_checkbox);
+        mMusicCheckbox.setChecked(true); //mặc định là true
+        btn_ok = findViewById(R.id.btn_ok);
+
+        LinearLayout linearLayout_credits = findViewById(R.id.linearLayout_credits);
+        linearLayout_credits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, CreditsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout linearLayout_policy = findViewById(R.id.linearLayout_policy);
+        linearLayout_policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, CreditsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainScreen();
+            }
+        });
+
+    }
+
+    private void goToMainScreen() {
+
+        Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+//        Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+//        startActivity(intent);
+        goToMainScreen();
+
+    }
+
+    public void viewClickHandler(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.music_checkbox:
+                switchMusicEnableCheckbox();
+                break;
+        }
+
+    }
+
+    private void switchMusicEnableCheckbox() {
+        //switch checkbox
+        isMusicOn = !isMusicOn;
+        if (isMusicOn) {
+            SettingPreferences.setMusicEnableDisable(mContext, true);
+            AppController.playMusic(); //bật
+        } else {
+            SettingPreferences.setMusicEnableDisable(mContext, false);
+            AppController.stopSound(); //tắt
+        }
+
+        populateMusicEnableContents();
+    }
+
+    protected void populateMusicEnableContents() {
+        //bật tắt âm thanh
+        if (SettingPreferences.getMusicEnableDisable(mContext)) {
+            AppController.playMusic(); //chạy ngạc
+            mMusicCheckbox.setChecked(true);
+        } else {
+            AppController.stopSound(); //ngừng nhạc
+            mMusicCheckbox.setChecked(false);
+        }
+        isMusicOn = SettingPreferences.getMusicEnableDisable(mContext);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("BUGBUG","onStop() in SettingActivity");
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("BUGBUG","onDestroy() in SettingActivity");
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/quizzapp/quizzLDSection/Activities/GeographyLevelsActivity.java
@@ -0,0 +1,174 @@
+package com.example.quizzapp.quizzLDSection.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.quizzapp.R;
+import com.example.quizzapp.quizzLDSection.constants.Constant;
+import com.example.quizzapp.quizzLDSection.model.Questions;
+
+public class GeographyLevelsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /*
+     *   Khong the su dung 1 LevelActivity cho toan bo the loai vi se phat sinh loi
+     *   1 Activity chi co the su dung cho 1 the loai
+     */
+
+    Button btnLevel1, btnLevel2, btnLevel3;
+
+    String CategoryValue = "";
+
+    int GL1, GL2, GL3; //Geography Levels
+
+    TextView tvLevel1, tvLevel2, tvLevel3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geography_levels);
+
+        btnLevel1 = findViewById(R.id.btnLevel1);
+        btnLevel2 = findViewById(R.id.btnLevel2);
+        btnLevel3 = findViewById(R.id.btnLevel3);
+
+//        btnLevel1.setOnClickListener(this);
+//        btnLevel2.setOnClickListener(this);
+//        btnLevel3.setOnClickListener(this);
+
+        tvLevel1 = findViewById(R.id.tvLevel1);
+        tvLevel2 = findViewById(R.id.tvLevel2);
+        tvLevel3 = findViewById(R.id.tvLevel3);
+
+
+        lockAndUnlockLevels();
+
+
+        Intent intentCategory = getIntent();
+        CategoryValue = intentCategory.getStringExtra("Category");
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (CategoryValue.equals("Geography")) {
+
+            switch (v.getId()) {
+
+                case R.id.btnLevel1:
+
+                    Intent intentGeoLevel1 = new Intent(GeographyLevelsActivity.this, QuizActivity.class);
+                    intentGeoLevel1.putExtra("Category", Questions.CATEGORY_GEOGRAPHY);
+                    intentGeoLevel1.putExtra("Level", Questions.LEVEL1);
+                    startActivity(intentGeoLevel1);
+                    break;
+
+                case R.id.btnLevel2:
+
+                    Intent intentGeoLevel2 = new Intent(GeographyLevelsActivity.this, QuizActivity.class);
+                    intentGeoLevel2.putExtra("Category", Questions.CATEGORY_GEOGRAPHY);
+                    intentGeoLevel2.putExtra("Level", Questions.LEVEL2);
+                    startActivity(intentGeoLevel2);
+                    break;
+
+                case R.id.btnLevel3:
+
+                    Intent intentGeoLevel3 = new Intent(GeographyLevelsActivity.this, QuizActivity.class);
+                    intentGeoLevel3.putExtra("Category", Questions.CATEGORY_GEOGRAPHY);
+                    intentGeoLevel3.putExtra("Level", Questions.LEVEL3);
+                    startActivity(intentGeoLevel3);
+                    break;
+            }
+        }
+
+    }
+
+    public void LoadData(View view) {
+
+        tvLevel1.setText(String.valueOf(GL1));
+        tvLevel2.setText(String.valueOf(GL2));
+        tvLevel3.setText(String.valueOf(GL3));
+
+    }
+
+    private void lockAndUnlockLevels() {
+
+        SharedPreferences sharedPreferences =
+                getSharedPreferences(getPackageName() + Constant.MY_LEVEL_PREFFILE,
+                        Context.MODE_PRIVATE);
+
+        GL1 = sharedPreferences.getInt(Constant.KEY_GEO_LEVEL_1,0);
+        GL2 = sharedPreferences.getInt(Constant.KEY_GEO_LEVEL_2,0);
+        GL3 = sharedPreferences.getInt(Constant.KEY_GEO_LEVEL_3,0);
+
+        if (GL1 == 1) {
+
+            btnLevel1.setClickable(true);
+            btnLevel1.setBackground(ContextCompat.getDrawable(this, R.drawable.button_background));
+            btnLevel1.setOnClickListener(this);
+        } else if (GL1 == 0) {
+            btnLevel1.setClickable(false);
+            btnLevel1.setBackground(ContextCompat.getDrawable(this, R.drawable.confirm_button_background));
+        }
+
+        if (GL2 == 1) {
+
+            btnLevel2.setClickable(true);
+            btnLevel2.setBackground(ContextCompat.getDrawable(this, R.drawable.button_background));
+            btnLevel2.setOnClickListener(this);
+
+        } else if (GL2 == 0) {
+
+            btnLevel2.setClickable(false);
+            btnLevel2.setBackground(ContextCompat.getDrawable(this, R.drawable.confirm_button_background));
+
+        }
+
+        if (GL3 == 1) {
+
+            btnLevel3.setClickable(true);
+            btnLevel3.setBackground(ContextCompat.getDrawable(this, R.drawable.button_background));
+            btnLevel3.setOnClickListener(this);
+
+        } else if (GL3 == 0) {
+
+            btnLevel3.setClickable(false);
+            btnLevel3.setBackground(ContextCompat.getDrawable(this, R.drawable.confirm_button_background));
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("BUGBUG","onStop() in GeographyLevelsActivity");
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("BUGBUG","onDestroy() in GeographyLevelsActivity");
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(GeographyLevelsActivity.this, CategoryActivity.class);
+        startActivity(intent);
+        finish();
+
+        super.onBackPressed();
+    }
+}
